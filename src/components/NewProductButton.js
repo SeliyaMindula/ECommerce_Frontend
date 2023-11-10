c409b741155ca5c4c82d8e858f94dbb898656308
seliyak <seliyak@yaratechnologies.com>
2023-11-10 11:25:05 +0530

    productUI changers

--- src/components/NewProductButton.js
+++ src/components/NewProductButton.js
@@ -3,9 +3,11 @@ import './NewProductButton.css';  // Make sure to create and import the CSS file
 
 const NewProductButton = ({ onAddProduct }) => {
   return (
+    <div className='pdl'>
     <button className="new-product-button" onClick={onAddProduct}>
       New Product
     </button>
+    </div>
   );
 };
 
